c34120ebba7afe8843066db2a8e12b3e81373ade
Jonathan Dursi <jonathan@dursi.ca>
2016-09-08 19:58:19 -0400

    Delete now-unnecessary install scripts

--- install-irkernel.R
+++
@@ -1,5 +0,0 @@
-install.packages(c('rzmq','repr','IRkernel','IRdisplay'),
-                 repos = c('http://irkernel.github.io/',
-                           'http://cran.utstat.utoronto.ca/'))
-
-IRkernel::installspec()
